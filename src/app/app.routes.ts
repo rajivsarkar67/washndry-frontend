214a67aaf40df7fdae09e8b35d72259af944da4f
rajivsarkar67 <rajiv.sarkar67@gmail.com>
2024-08-01 11:11:04 +0530

    feat: basic layout for landing and address

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { OrdersListComponent } from './orders-list/orders-list.component';
+import { AddressComponent } from './address/address.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: LandingComponent},
+    {path: 'orders-list', component: OrdersListComponent},
+    {path: 'address', component: AddressComponent},
+
+];
