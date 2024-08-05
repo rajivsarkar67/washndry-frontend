bfa37b09deaa44681bec248db2a9a6290c0082af
rajivsarkar67 <rajiv.sarkar67@gmail.com>
2024-08-05 11:29:26 +0530

    feat: started working on selection page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { LandingComponent } from './landing/landing.component';
 import { OrdersListComponent } from './orders-list/orders-list.component';
 import { AddressComponent } from './address/address.component';
 import { ScheduleComponent } from './schedule/schedule.component';
+import { SelectionComponent } from './selection/selection.component';
 
 export const routes: Routes = [
     {path: '', component: LandingComponent},
+    {path: 'selection', component: SelectionComponent},
     {path: 'schedule', component: ScheduleComponent},
     {path: 'address', component: AddressComponent},
     {path: 'orders-list', component: OrdersListComponent},
-
 ];
