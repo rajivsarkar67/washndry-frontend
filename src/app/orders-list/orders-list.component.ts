26c11711f13f3d6076378eaa6c9de21e3714197d
rajivsarkar67 <rajiv.sarkar67@gmail.com>
2025-02-21 22:42:04 +0530

    test: get-orders api integrated

--- src/app/orders-list/orders-list.component.ts
+++ src/app/orders-list/orders-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { HeaderComponent } from "../header/header.component";
 import { DataService } from '../data.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-orders-list',
@@ -12,7 +13,16 @@ import { DataService } from '../data.service';
 })
 export class OrdersListComponent {
 
-  constructor(private router: Router, public dataService: DataService){}
+  constructor(private router: Router, public dataService: DataService, private http: HttpClient){}
+
+  ngOnInit(){
+    const headers = { 'Authorization': 'Bearer '+ this.dataService.authToken };
+    this.http.get('http://localhost:5000/api/orders', {headers}).subscribe(res => {
+      console.log(res);
+    }, (error)=>{
+      alert(error.error.message);
+    });
+  }
 
   goToSelection(){
     this.router.navigate(['selection']);
